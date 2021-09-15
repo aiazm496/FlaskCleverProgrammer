8b30eb0c30c9d3591d91dca70b47b35c5edc15eb
Akash <aiazm496@gmail.com>
2021-09-16 00:59:58 +0530

    15thSept

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import re
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'  #sqlite stories db locally at location of app.py
@@ -11,11 +13,11 @@ db = SQLAlchemy(app)
 class BlogPost(db.Model): #BlogPost inherited the db.Model class
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(100), nullable = False)
-    content = db.Column(db.Text, nullable = False)
+    content = db.Column(db.Text, nullable = False)                                                         
     author = db.Column(db.String(20), nullable = False, default = 'N/A')
     date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
 
-    def __repr__(self):
+    def __repr__(self):             #like java toString
         return 'Blog post ' + str(self.id)
 
 
@@ -38,12 +40,28 @@ def index():
     return render_template('index.html') #it auto checks in templates folder
     #and renders the index.html on route '/'
 
-@app.route('/posts')
+@app.route('/posts',methods = ['GET','POST'])
 def posts():
-    return render_template('posts.html', posts = all_posts)
+    if request.method == 'POST':  #request is what is sent by form
+        post_title = request.form['title']
+        post_content = request.form['content']
+        new_post = BlogPost(title = post_title, content = post_content,author = 'Akash')
+
+        #adding to db from form
+        db.session.add(new_post)
+        db.session.commit()  #to add permanently
+        return redirect('/posts')  #url to return back
+
+    else:
+        all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
+        return render_template('posts.html', posts = all_posts)
 # we have defined variable posts here to store all_data
 # now we have write code in html to display it on posts.html
-
+#from app import db,BlogPost
+# get BlogPost objecy by id obj = BlogPost.query.get(1)  enter id number
+#to filter list of BlogPost object -> BlogPost.query.filter_by(title = 'Modi').all()
+#to get title  of id 1-> BlogPost.query.get(1).title
+#to change BlogPost.query.get(1).title = 'Narendra Modi' and db.session.commit()
 
 #@app.route('/') #nothing in url it is like localhost:5000/
 @app.route('/home/<string:name>')  # it is like localhost:5000/home
@@ -57,7 +75,6 @@ def helloUser(name,id):
 @app.route('/onlyget',methods = ['GET'])
 def get_req():
     return "You can only get this webpage"
-
 
 if __name__ == '__main__':  #to see messages in cmd
     app.run(debug=True) 
